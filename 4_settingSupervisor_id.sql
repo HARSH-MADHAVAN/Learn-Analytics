65ca2fdd4d94a1472daec9ca7463eb0fd5e09842
HARSH-MADHAVAN <harshmadhavan2003@gmail.com>
2025-01-11 00:18:40 +0530

    Inserting table Data

--- 4_settingSupervisor_id.sql
+++ 4_settingSupervisor_id.sql
@@ -6,4 +6,9 @@ ALTER table employee
 ADD foreign key(branch_id)
 references branch (branch_id)
 ON DELETE SET NULL;
+
+ALTER table employee 
+ADD foreign key(super_id)
+references employee (emp_id)
+ON DELETE SET NULL;
 	
