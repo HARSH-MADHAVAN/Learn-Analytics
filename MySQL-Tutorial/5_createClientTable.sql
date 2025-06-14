ea9e17a663dfb90f91d4f1647b06e920b0805c42
HARSH-MADHAVAN <harshmadhavan2003@gmail.com>
2025-06-14 14:50:13 +0530

    adding MySQL

---
+++ MySQL-Tutorial/5_createClientTable.sql
@@ -0,0 +1,9 @@
+use company;
+
+CREATE table client (
+	client_id INT PRIMARY KEY,
+    client_name VARCHAR(40),
+    branch_id INT,
+    
+    FOREIGN KEY(branch_id)  REFERENCES branch(branch_id) ON DELETE SET NULL
+);
