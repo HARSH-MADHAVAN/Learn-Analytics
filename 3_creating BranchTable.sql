8694d75ded33bc2078ad4e9cdb59b67f5a849649
HARSH-MADHAVAN <harshmadhavan2003@gmail.com>
2025-06-14 14:52:05 +0530

    deleted files

--- 3_creating BranchTable.sql
+++
@@ -1,9 +0,0 @@
-use company;
-CREATE table branch (
-	branch_id INT PRIMARY KEY,
-    branch_name VARCHAR(40),
-    mgr_id INT,
-    mgr_start_date DATE,
-    
-    FOREIGN KEY(mgr_id)  REFERENCES employee(emp_id) ON DELETE SET NULL
-);
--- 5_createClientTable.sql
+++
@@ -1,9 +0,0 @@
-use company;
-
-CREATE table client (
-	client_id INT PRIMARY KEY,
-    client_name VARCHAR(40),
-    branch_id INT,
-    
-    FOREIGN KEY(branch_id)  REFERENCES branch(branch_id) ON DELETE SET NULL
-);
